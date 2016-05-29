e4b131625a1a11dc1b647dfdd892d34d458032fb
domingospawn <domingospawn@gmail.com>
2016-05-29 16:39:30 -0300

    Relationship User and Joke

--- app/Http/Controllers/JokesController.php
+++ app/Http/Controllers/JokesController.php
@@ -3,19 +3,49 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Illuminate\Http\Response;
 use App\Http\Requests;
-
 use App\Joke;
+use App\User;
 
 class JokesController extends Controller
 {
+	private function transformCollection($jokes)
+	{
+		return array_map([$this, 'transform'], $jokes->toArray());
+	}
+
+	private function transform($joke)
+	{
+		return [
+			'joke_id' => $joke['id'],
+			'joke' => $joke['body']
+		];
+	}
+
     public function index()
     {
     	$jokes = Joke::all(); //Not a good idea when database grows up
-    	return $jokes;
-//    	return Response::json([
-//    		'message' => $jokes
-//    	], 200);
+    	return response()->json([
+    		'data' => $this->transformCollection($jokes),
+    		'status' => 200
+    	]);
+    }
+
+    public function show($id)
+    {
+    	$joke = Joke::find($id);
+
+    	if(!$joke){
+    		return response()->json([
+    			'error' => ['message'=>'Joke does not exist'],
+    			'status' => 404
+    		]);
+    	}
+
+    	return response()->json([
+    		'data' => $this->transform($joke),
+    		'status' => 200
+    	]);
     }
 }
