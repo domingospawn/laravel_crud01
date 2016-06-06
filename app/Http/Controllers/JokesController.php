c331e1d180d83fec4b4be88677886bed038cfc12
domingospawn <domingospawn@gmail.com>
2016-06-06 18:33:47 -0300

    To restart

--- app/Http/Controllers/JokesController.php
+++ app/Http/Controllers/JokesController.php
@@ -19,7 +19,9 @@ class JokesController extends Controller
 	{
 		return [
 			'joke_id' => $joke['id'],
-			'joke' => $joke['body']
+			'joke' => $joke['body'],
+//          'submitted_by' => $joke['user_id']
+            'submitted_by' => var_dump($joke['user']);
 		];
 	}
 
@@ -34,18 +36,74 @@ class JokesController extends Controller
 
     public function show($id)
     {
-    	$joke = Joke::find($id);
+        $joke = Joke::with(
+            array('User'=>function($query){
+                $query->select('id','name');
+            })
+        )->find($id);        
 
-    	if(!$joke){
+    	if(!$joke)
+        {
     		return response()->json([
     			'error' => ['message'=>'Joke does not exist'],
     			'status' => 404
     		]);
     	}
 
-    	return response()->json([
-    		'data' => $this->transform($joke),
-    		'status' => 200
-    	]);
+        //get previous joke id
+        $previous = Joke::where('id', '<', $joke->id)->max('id');
+
+        //get next joke id
+        $next = Joke::where('id', '>', $joke->id)->min('id');
+
+        return response()->json([
+            'previous_joke_id' => $previous,
+            'next_joke_id' => $next,
+            'data' => $this->transform($joke),
+            'status' => 200
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+        if(!$request->body or !$request->user_id)
+        {
+            return response()->json([
+                'error' => ['message'=>'Please provide both body and user_id'],
+                'status' => 422
+            ]);
+        }
+
+        $joke = Joke::create($request->all());
+
+        return response()->json([
+            'message' => 'Joke created succefully',
+            'data' => $this->transform($joke)
+        ]);
+    }
+
+    public function update(Request $request, $id)
+    {
+        if(!$request->body or !$request->user_id)
+        {
+            return response()->json([
+                'error' => ['message'=>'Please provide both body and user_id'],
+                'status' => 422
+            ]);
+        }
+
+        $joke = Joke::find($id);
+        $joke->body = $request->body;
+        $joke->user_id = $request->user_id;
+        $joke->save();
+
+        return response()->json([
+            'message'=>'Joke updated succefully'
+        ]);
+    }
+
+    public function destroy($id)
+    {
+        Joke::destroy($id);
     }
 }
